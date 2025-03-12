e30f804512e1ea5dcd1ce53f4e91e1a7eea893f9
HubertKw <102217312+HubertKw@users.noreply.github.com>
2025-03-13 00:48:01 +0100

    Delete firefighter.h

--- firefighter.h
+++
@@ -1,8 +0,0 @@
-// Plik: firefighter.h
-#ifndef FIREFIGHTER_H
-#define FIREFIGHTER_H
-
-// Deklaracja funkcji uruchamiającej proces strażaka
-int start_firefighter(int shm_id, int sem_id);
-
-#endif
